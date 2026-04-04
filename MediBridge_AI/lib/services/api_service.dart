06753b5ad56566238b7e5c8268ebebb6775a183d
Kalash Rao <kalash.rao07@gmail.com>
2026-04-05 05:17:45 +0530

    Add ApiService class with configuration constants

---
+++ MediBridge_AI/lib/services/api_service.dart
@@ -0,0 +1,13 @@
+class ApiService {
+
+  // 🔴 REPLACE THESE WITH YOUR FRIEND'S VALUES
+
+  static const String endpoint = "https://sgp.cloud.appwrite.io/v1";
+
+  static const String projectId = "69c8f07c00157b2d6d86";
+
+  static const String databaseId = "69c8f0ac00115c11cfe0";
+
+  static const String patientsCollectionId = "patients";
+
+}
