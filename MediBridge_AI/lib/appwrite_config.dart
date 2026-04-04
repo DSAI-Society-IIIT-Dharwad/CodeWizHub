d73d187ee85f46558cb7a60b4c0e2d4844d68e15
Kalash Rao <kalash.rao07@gmail.com>
2026-04-05 05:22:43 +0530

    Add Appwrite configuration for uploads collection

---
+++ MediBridge_AI/lib/appwrite_config.dart
@@ -0,0 +1,11 @@
+class AppwriteConfig {
+  static const String endpoint = 'https://sgp.cloud.appwrite.io/v1';
+  static const String projectId = '69c8f07c00157b2d6d86';
+  static const String databaseId = '69c8f0ac00115c11cfe0';
+  static const String patientsCollection = 'patients';
+  static const String doctorsCollection = 'doctors';
+  static const String studentsCollection = 'students';
+  static const String consultationsCollection = 'consultations';
+  static const String uploadsCollection = 'uploads'; // ← NEW
+  static const String apiKey = 'standard_986ff6425985217bdd8942d640879ccbfd9f21a4a2d34dded1e5713ae7ed0898e746fbabd36047fa918f765d99f56b3f061b8b9066f4747333da17a571a021398a3d1e61c172153d60858f6eacd86f6f3454ae58fb45fe2cfb1b0118e39efdcebcdd1b69c57897598e8ef4a07d2c59e0adbe77e05cdf590c165b5802d2919ba4';
+}
